c6c220f22a7f4c496cc1d127390522422e5bb6cd
Simon Bolduc <sbolduc@8d.com>
2016-05-10 22:42:47 -0400

    added db migration

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+require( 'dotenv' ).config();
+
+var database = {
+    client: 'postgresql',
+    connection: process.env.POSTGRES_URL,
+    migrations: {
+        tableName: 'migrations'
+    }
+};
+
+module.exports = {
+
+    development: database,
+
+    staging: database,
+
+    production: database
+
+};
---
+++ migrations/20160510221513_initial.js
@@ -0,0 +1,33 @@
+exports.up = function ( knex, promise ) {
+    return promise.all( [knex.schema.createTable( 'configuration', function ( t ) {
+        t.increments();
+        t.time( 'time' ).notNullable();
+        t.specificType( 'twoplayersgames', 'VARCHAR[]' ).notNullable();
+        t.specificType( 'threeplayersgames', 'VARCHAR[]' ).notNullable();
+        t.specificType( 'fourplayersgames', 'VARCHAR[]' ).notNullable();
+        t.specificType( 'fiveplayersgames', 'VARCHAR[]' ).notNullable();
+        t.specificType( 'sixplayersgames', 'VARCHAR[]' ).notNullable();
+        t.specificType( 'sevenplayersgames', 'VARCHAR[]' ).notNullable();
+        t.integer( 'answerdelaymin' ).notNullable();
+    } ), knex( 'configuration' ).insert( {
+        time: '11:45:00',
+        twoplayersgames: '{}',
+        threeplayersgames: '{}',
+        fourplayersgames: '{}',
+        fiveplayersgames: '{}',
+        sixplayersgames: '{}',
+        sevenplayersgames: '{}',
+        answerdelaymin: '15'
+    } ),
+     knex.schema.createTable( 'games', function ( t ) {
+        t.increments();
+        t.integer( 'players' ).defaultTo( '0' ).notNullable();
+        t.timestamp( 'date' ).defaultTo( 'now()' ).notNullable();
+        t.text( 'messageid' ).notNullable();
+        t.text( 'channelid' ).notNullable();
+    } )] );
+};
+
+exports.down = function ( knex, promise ) {
+    return promise.all( [knex.schema.dropTable( 'configuration' ), knex.schema.dropTable( 'games' )] );
+};
